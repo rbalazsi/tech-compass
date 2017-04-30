e5ae452d9c2b60f3e038af345235d9dcabde5465
Robert Balazsi <contact@robertbalazsi.com>
2017-04-30 23:46:46 +0300

    Implemented CVS-based data provider mechanism for TestNG.

---
+++ testng-csv/src/main/java/com/robertbalazsi/techcompass/testngcsv/Calculator.java
@@ -0,0 +1,36 @@
+package com.robertbalazsi.techcompass.testngcsv;
+
+import java.util.stream.IntStream;
+
+/**
+ * The subject class, a simple calculator that can perform several types of arithmetic operations having exactly two operands.
+ */
+public class Calculator {
+
+    public static double calc(double n1, String operation, double n2) {
+        switch (operation) {
+            case "+":
+                return n1 + n2;
+            case "-":
+                return n1 - n2;
+            case "*":
+                return n1 * n2;
+            case "/":
+                return n1 / n2;
+            case "%":
+                return n1 % n2;
+            case "^":
+                return Math.pow(n1, n2);
+            default:
+                throw new UnsupportedOperationException(String.format("Arithmetic operation %s is not supported.", operation));
+        }
+    }
+
+    public static int arraySum(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        return IntStream.of(numbers).sum();
+    }
+}
---
+++ testng-csv/src/main/java/com/robertbalazsi/techcompass/testngcsv/framework/CSVDataSource.java
@@ -0,0 +1,32 @@
+package com.robertbalazsi.techcompass.testngcsv.framework;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+/**
+ * Annotation used on a @Test method to associate a CSV resource as data provider to it.
+ */
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({METHOD, TYPE, CONSTRUCTOR})
+public @interface CSVDataSource {
+
+    /**
+     * The {@link java.net.URL URL} of the CSV, given as a string. It needs to be a classpath resource relative to the test class.
+     */
+    String url() default "";
+
+    /**
+     * Indicates whether the first line in the CSV is considered the header (for easier readability using some tools),
+     * and thereby ignored by the test runner.
+     */
+    boolean hasHeader() default false;
+
+    /**
+     * The string to be used as the separator of array elements.
+     */
+    String arrayElementSeparator() default ",";
+}
