3a510f8ba841d6c7a290c97c6b693f626db19b03
Robert Balazsi <contact@robertbalazsi.com>
2017-05-21 12:50:39 +0300

    Implemented support for toggling between 2FA enable/disable, added some TODOs for future improvements.

--- two-factor-auth/src/main/java/com/robertbalazsi/techcompass/twofactorauth/account/TwoFactorService.java
+++ two-factor-auth/src/main/java/com/robertbalazsi/techcompass/twofactorauth/account/TwoFactorService.java
@@ -5,8 +5,10 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.robertbalazsi.techcompass.twofactorauth.config.ApplicationProperties;
 import org.apache.commons.codec.binary.Base32;
 import org.apache.commons.codec.binary.Hex;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.FileNotFoundException;
@@ -15,6 +17,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.SecureRandom;
 
 /**
@@ -22,6 +26,10 @@ import java.security.SecureRandom;
  */
 @Service
 public class TwoFactorService {
+
+    @Autowired
+    private ApplicationProperties appProps;
+
     public static final String ISSUER = "2FA Demo";
     public static final int DEFAULT_TOTP_INTERVAL_SECS = 30;
     private static final String UTF_8 = "UTF-8";
@@ -42,16 +50,21 @@ public class TwoFactorService {
         return TOTP.generateTOTP(Hex.encodeHexString(decodedBytes), Long.toHexString(time), "6");
     }
 
-    public void generateQRCodePNG(String secret, String account, String path, int width, int height) {
+    public void generateQRCodePNG(String secret, String account, int width, int height) {
         String barCodeLink = getGoogleAuthenticatorBarLink(secret, account);
         try {
             BitMatrix bitMatrix = new MultiFormatWriter().encode(barCodeLink, BarcodeFormat.QR_CODE, width, height);
+            String path = getBarcodePath(secret);
             try (FileOutputStream out = new FileOutputStream(path)) {
                 MatrixToImageWriter.writeToStream(bitMatrix, "png", out);
             }
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private String getBarcodePath(String secret) {
+        return appProps.getFilesDir() + "/" + secret + ".png";
     }
 
     private static String getGoogleAuthenticatorBarLink(String secret, String account) {
@@ -63,6 +76,14 @@ public class TwoFactorService {
                     + "&issuer=" + URLEncoder.encode(ISSUER, UTF_8)).replace("+", "%20");
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    public void cleanupQRCodePNG(String secret) {
+        try {
+            Files.deleteIfExists(Paths.get(getBarcodePath(secret)));
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not clean up QR barcode PNG for secret: " + secret, e);
         }
     }
 }
--- two-factor-auth/src/main/java/com/robertbalazsi/techcompass/twofactorauth/controller/TwoFactorConfigController.java
+++ two-factor-auth/src/main/java/com/robertbalazsi/techcompass/twofactorauth/controller/TwoFactorConfigController.java
@@ -9,17 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/2faconf")
 public class TwoFactorConfigController {
-
-    @Autowired
-    private ApplicationProperties appProps;
 
     @Autowired
     private TwoFactorService twoFactorService;
@@ -33,29 +32,61 @@ public class TwoFactorConfigController {
     @RequestMapping(method = RequestMethod.GET)
     public String twoFactorConfig(Model model) {
         Account account = userService.getCurrentAccount();
-        String secret;
-        if (account.isTwoFactorEnabled()) {
-            secret = account.getTwoFactorSecret();
-        } else {
-            secret = twoFactorService.generateBase32SecretKey();
-            account.setTwoFactorEnabled(true);
+        boolean twoFactorEnabled = account.isTwoFactorEnabled();
+        model.addAttribute("twoFactorEnabled", twoFactorEnabled);
+        if (!account.isTwoFactorEnabled()) {
+            String secret = twoFactorService.generateBase32SecretKey();
             account.setTwoFactorSecret(secret);
             userService.updateAccount(account);
+            twoFactorService.generateQRCodePNG(secret, account.getEmail(), 400, 400);
 
-            twoFactorService.generateQRCodePNG(secret, account.getEmail(), appProps.getFilesDir() + "/" + secret + ".png", 400, 400);
+            model.addAttribute("qrPath", "/files/" + secret + ".png");
+            model.addAttribute("base32Secret", secret);
         }
-
-        model.addAttribute("qrPath", "/files/" + secret + ".png");
-        model.addAttribute("base32Secret", secret);
 
         return "2faconf";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String logout(HttpServletRequest request) {
+    @RequestMapping(path = "/action", method = RequestMethod.POST)
+    public String action(HttpServletRequest request, @RequestParam(name = "action") String action) {
+        Account account = userService.getCurrentAccount();
+        boolean twoFactorEnabled = account.isTwoFactorEnabled();
+
         try {
-            request.logout();
-            return "redirect:/login";
+            switch (action) {
+                case "ok":
+                    // Confirm enabling 2FA:
+                    if (!twoFactorEnabled) {
+                        if (account.getTwoFactorSecret() == null) {
+                            return "redirect:/login";
+                        }
+                        account.setTwoFactorEnabled(true);
+                        userService.updateAccount(account);
+                        request.logout();
+                        return "redirect:/login";
+                    }
+                    // Confirm disabling 2FA:
+                    else {
+                        String secret = account.getTwoFactorSecret();
+                        twoFactorService.cleanupQRCodePNG(secret);
+                        account.setTwoFactorEnabled(false);
+                        account.setTwoFactorSecret(null);
+                        userService.updateAccount(account);
+                        return "redirect:/home";
+                    }
+                case "cancel":
+                    String secret = account.getTwoFactorSecret();
+                    // Cancel enabling 2FA
+                    if (!account.isTwoFactorEnabled() && secret != null) {
+                        twoFactorService.cleanupQRCodePNG(secret);
+                        account.setTwoFactorSecret(null);
+                        userService.updateAccount(account);
+                    }
+                    return "redirect:/home";
+                default:
+                    throw new IllegalStateException("Invalid action: " + action);
+            }
+
         } catch (ServletException e) {
             throw new IllegalStateException(e);
         }
